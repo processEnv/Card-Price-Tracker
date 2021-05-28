5d54c7fc4bc071da9434355a09d1a944f6c06e4a
Matthew Leng <mleng89@gmail.com>
2021-05-27 20:47:07 -0400

    Set mode to development

--- webpack.config.js
+++ webpack.config.js
@@ -6,47 +6,47 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 // of a template file that this plugin will use.
 // We will create it later.
 const htmlPlugin = new HtmlWebPackPlugin({
-	template: './src/index.html',
+    template: './src/index.html'
 });
 
 module.exports = {
-	// Our application entry point.
-	entry: './src/index.tsx',
+    // Our application entry point.
+    entry: './src/index.tsx',
+    mode: 'development',
+    // These rules define how to deal
+    // with files with given extensions.
+    // For example, .tsx files
+    // will be compiled with ts-loader,
+    // a spcific loader for webpack
+    // that knows how to work with TypeScript files.
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
 
-	// These rules define how to deal
-	// with files with given extensions.
-	// For example, .tsx files
-	// will be compiled with ts-loader,
-	// a spcific loader for webpack
-	// that knows how to work with TypeScript files.
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			},
-		],
-	},
+    // Telling webpack which extensions
+    // we are interested in.
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
 
-	// Telling webpack which extensions
-	// we are interested in.
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js'],
-	},
+    // What file name should be used for the result file,
+    // and where it should be palced.
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
 
-	// What file name should be used for the result file,
-	// and where it should be palced.
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-	},
-
-	// Use the html plugin.
-	plugins: [htmlPlugin],
-	devServer: {
-		contentBase: path.join(__dirname, 'dist'),
-		compress: true,
-		port: 3000,
-	},
+    // Use the html plugin.
+    plugins: [htmlPlugin],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 3000
+    }
 };
